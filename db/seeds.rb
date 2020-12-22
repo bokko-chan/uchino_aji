d88772563ae3ac6e13e39b06441dd5cdc9ee3756
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-12-22 21:57:43 +0900

    seedsからDBへ調味料登録確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+seasonings = %w(
+  塩 岩塩 藻塩
+  砂糖 白砂糖 グラニュ糖 三温糖 きび砂糖 黒砂糖 氷砂糖
+  醤油 濃口醤油 薄口醤油 甘口醤油 溜醤油 白醤油 だし醤油
+)
+
+seasonings.each do |seasoning|
+  Seasoning.find_or_create_by(name: seasoning)
+end
