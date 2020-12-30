42e4405e56586d7717aa79c3fb4fec8ca6181a8a
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-12-30 16:46:56 +0900

    routing検証

--- app/controllers/seasonings_controller.rb
+++ app/controllers/seasonings_controller.rb
@@ -7,23 +7,28 @@ class SeasoningsController < ApplicationController
 
   def new
     @seasoning = Seasoning.new
-    @user_seasoning = @seasoning.user_seasonings.build
+    # @user_seasoning = @seasoning.user_seasonings.build
     # @user = current_user
+    # @seasoning = Seasoning.find(params[:seasoning_id])
+    # @user_seasoning = UserSeasoning.new
   end
 
   def create
     # binding.pry
-    
+    @seasoning = Seasoning.new(seasoning_params)
+    # @seasoning.user_id = current_user.id
+    # @seasoning = Seasoning.find(params[:seasoning_id])
+    # @user_seasoning = UserSeasoning.new(seasoning_params)
     # @user_seasoning = UserSeasoning.includes(:seasonings).where(user_id: current_user.id)
     # @user_seasoning = Seasoning.includes(:user).where(user_id: current_user.id)
-    @user_seasoning = Seasoning.includes(:user_seasonings).where(user_id: current_user.id)
+    # @user_seasoning = Seasoning.includes(:user_seasonings).where(user_id: current_user.id)
     # @seasoning = Seasoning.includes(user_id: current_user.id).create(seasoning_params)
     # @seasoning = @user_seasoning.build(seasoning_params)
     # @user = current_user
     # @user_seasoning = @user.seasonings.build(seasoning_params)
     # @user = User.find(params[:user_id])
     # @seasoning = @user.seasonings.create(seasoning_params)
-    @seasoning = @user_seasoning.create(seasoning_params)
+    # @seasoning = @user_seasoning.create(seasoning_params)
     if @seasoning.save
       # binding.pry
       redirect_to action: :index
@@ -35,7 +40,7 @@ class SeasoningsController < ApplicationController
   private
   
   def seasoning_params
-    params.require(:seasoning).permit(:name, :user_created_at, :created_at, :updated_at, user_ids: [], user_seasonings_attributes: [seasoning_ids: []], seasonings: {}).merge(user_id: current_user.id)
+    params.require(:seasoning).permit(:name, :user_created_at, :created_at, :updated_at, :user_id, user_ids: [], seasoning_ids: [], seasonings: {}).merge(user_id: current_user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,22 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'seasonings#index'
   get 'rakuten_search' => 'seasonings#index'
-  resources :seasonings, only:[:index, :new, :create]
-  resources :users, only:[:show]
+
+  resources :users, :seasonings, only:[:index, :new, :create, :show] do
+    resources :user_seasonings
+  end
+
+  # resources :users, only:[:show]
+  # resources :seasonings, only:[:index, :new, :create] do
+  #   member do
+  #     get :users
+  #   end
+  # end
+
+  # resources :users do
+  #   resources :seasonings
+  # end
+
+
+  # resources :users, only:[:show]
 end
