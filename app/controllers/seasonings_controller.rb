30d685633ff9b657d70bb224480d63b06915b286
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2021-01-09 21:45:22 +0900

    Revert "Revert "association, seasonings controller build記述検証""
    This reverts commit 53af9fb4b1f429c5ff2fc0c61a05b6f1e3897e10.

--- app/controllers/seasonings_controller.rb
+++ app/controllers/seasonings_controller.rb
@@ -7,55 +7,61 @@ class SeasoningsController < ApplicationController
 
   def new
     @seasoning = Seasoning.new
-    @user = User.find(current_user.id) 
     # @user_seasoning = @seasoning.user_seasonings.build
-    # @user = current_user
-    # @seasoning = Seasoning.find(params[:seasoning_id])
-    # @user_seasoning = UserSeasoning.new
   end
 
   def create
-    # binding.pry
-    @user_seasonings = [] 
-    if seasoning_params[:seasonings][:name].present?
-      s = Seasoning.create(name: seasoning_params[:seasonings][:name])
+    binding.pry
+    # @user_seasonings = UserSeasoning.new
+    # seasoning = Seasoning.create(seasoning_params)
+    # @seasoning = @user_seasonings.build_seasoning(seasoning_params)
+    # @seasoning = @user_seasonings.seasonings.build(seasoning_params)
     
-      @user_seasonings << UserSeasoning.new(seasoning_id: s.id, user_id: seasoning_params[:user_id])
+    # @user_seasonings = seasoning.user_seasonings.build(seasoning_params)
     
-    end
-    
-    seasoning_params[:seasonings][:seasoning_ids].each do |sid|
-    @user_seasonings << UserSeasoning.new(seasoning_id: sid, user_id: seasoning_params[:user_id])
-    end
-    
-    # @seasoning = Seasoning.create(seasoning_params)
-    # @seasoning.user_id = current_user.id
-    # @seasoning = Seasoning.find(params[:seasoning_id])
-    # @user_seasoning = UserSeasoning.new(seasoning_params)
-    # @user_seasoning = UserSeasoning.includes(:seasonings).where(user_id: current_user.id)
-    # @user_seasoning = Seasoning.includes(:user).where(user_id: current_user.id)
-    # @user_seasoning = Seasoning.includes(:user_seasonings).where(user_id: current_user.id)
-    # @seasoning = Seasoning.includes(user_id: current_user.id).create(seasoning_params)
-    # @seasoning = @user_seasoning.build(seasoning_params)
     # @user = current_user
-    # @user_seasoning = @user.seasonings.build(seasoning_params)
-    # @user = User.find(params[:user_id])
-    # @seasoning = @user.seasonings.create(seasoning_params)
-    # @seasoning = @user_seasoning.create(seasoning_params)
-    if @user_seasonings.map(&:valid?).all?      
-      @user_seasonings.each do |us|
-        us.save
-      end
+    # @seasoning = @user.seasonings.build(seasoning_params)
+    
+    # user_seasonings = UserSeasoning.new
+    # @seasoning = Seasonig.new
+    @seasoning = Seasonig.find_or_created_by(seasoning_params)
+    @user_seasonings = @seasoning.user_seasonings.build({user_id: current_user.id}, {seasoning_id: @seasoning.id})
+    # @user_seasonings = UserSeasoning.build({user_id: current_user.id}, {seasoning_id: @seasoning.id})
+    
+    if @user_seasonings.save
       redirect_to action: :index
     else
       render :new
     end
   end
 
+
+
+
+  #   @user_seasonings = [] 
+  #   if seasoning_params[:seasonings][:name].present?
+  #     s = Seasoning.find_or_created_by(name: seasoning_params[:seasonings][:name])
+  #     @user_seasonings << UserSeasoning.new(seasoning_id: s.id, user_id: seasoning_params[:user_id])
+  #   end
+    
+  #   seasoning_params[:seasonings][:seasoning_ids].each do |sid|
+  #     @user_seasonings << UserSeasoning.new(seasoning_id: sid, user_id: seasoning_params[:user_id])
+  #   end
+    
+  #   if @user_seasonings.map(&:valid?).all?      
+  #     @user_seasonings.each do |us|
+  #       us.save
+  #     end
+  #     redirect_to action: :index
+  #   else
+  #     render :new
+  #   end
+  # end
+
   private
   
   def seasoning_params
-    params.require(:seasoning).permit(:name, :user_created_at, :created_at, :updated_at, :user_id, user_ids: [], seasoning_ids: [], seasonings: {}).merge(user_id: current_user.id)
+    params.require(:seasoning).permit(:name, :user_created_at, :created_at, :updated_at, :user_id, user_ids: [], :seasoning_id, seasoning_ids: [], seasonings: {}).merge(user_id: current_user.id)
   end
 
 end
